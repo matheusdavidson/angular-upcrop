48805d21c859c8db2c065aa5beae2770e6947197
Matheus Davidson <matheusdavidson@gmail.com>
2017-10-05 14:53:25 -0300

    chore(lib/update): update lib generator with playground

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,6 @@
 import { ModuleWithProviders } from '@angular/core';
 export * from './upcrop-upload/upcrop-upload.component';
+export * from './upcrop-upload-insta/upcrop-upload-insta.component';
 export * from './upcrop-crop/upcrop-crop.component';
 export * from './upcrop-dialog/upcrop-dialog.component';
 export * from './upcrop-image-preview/upcrop-image-preview.directive';
