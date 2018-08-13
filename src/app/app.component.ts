a7bb802b001caf53238c322153344e5f6a118759
Matheus Davidson <matheusdavidson@gmail.com>
2018-08-13 00:14:45 -0300

    chore(test): test current lib port

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { UpcropService } from 'angular-upcrop';
+import { environment } from '../environments/environment';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    providers: [UpcropService]
 })
 export class AppComponent {
-  title = 'app';
+
+    constructor(
+        public upcropService: UpcropService
+    ) { }
+
+    //
+    // Create a method in the component to trigger upcrop
+    openUpcrop() {
+        console.log('open');
+        //
+        // Set config for dialog, upload and crop components
+        let dialogConfig = {};
+        let uploadConfig = {
+            url: environment.api + 'events/upload',
+            additionalParameter: {}
+        };
+        let cropConfig = {};
+
+        //
+        // Open upcrop dialog
+        console.log('open start');
+        this.upcropService.open(dialogConfig, uploadConfig, cropConfig).then((data) => {
+            console.log('data', data);
+            //
+            // Data will contain an array with the images
+            data;
+        }).catch((err) => {
+            console.log('error', err);
+        });
+    }
 }
